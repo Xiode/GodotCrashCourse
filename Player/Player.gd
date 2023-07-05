94dfcebdffa3713cce4a1c14947e523a9c428ffe
Xiode <xiodetheone@gmail.com>
2023-07-04 20:42:44 -0600

    Player matches wall angle
    - Angle of Player's body now matches wall angle while climbing.
    - Added a RemoteTransform3D to keep the head connected while in a rotated state.

--- Player/Player.gd
+++ Player/Player.gd
@@ -17,13 +17,21 @@ signal weapon_switched(weapon_name: String)
 ## between the idle and running states.
 @export var stopping_speed := 1.0
 
+@export var hang_angle := 0.0
+
 @onready var _menu_root: CanvasLayer = $Menu
 @onready var _rotation_root: Node3D = $CharacterRotationRoot
 @onready var _camera_controller: CameraController = $CameraController
 @onready var _ground_shapecast: ShapeCast3D = $GroundShapeCast
+
+# TODO: Consider breaking all this climbing stuff out into its own class? Maybe this is fine.
 @onready var _climb_shapecast: ShapeCast3D = $ClimbShapeCast
 @onready var _climb_area: Area3D = $ClimbArea
 @onready var _head_root: Node3D = $HeadRoot
+@onready var _left_hand: Node3D = $CharacterRotationRoot/LeftHand
+@onready var _left_shoulder: Node3D = $CharacterRotationRoot/LeftShoulder
+@onready var _right_hand: Node3D = $CharacterRotationRoot/RightHand
+@onready var _right_shoulder: Node3D = $CharacterRotationRoot/RightShoulder
 
 @onready var _move_direction := Vector3.ZERO
 @onready var _last_strong_direction := Vector3.FORWARD
@@ -34,11 +42,13 @@ signal weapon_switched(weapon_name: String)
 @onready var _climbing_mode := false
 
 var wall_normal: Vector3
+var wall_relative_up: Vector3
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	_camera_controller.setup(self)
 	_camera_controller.set_pivot(_camera_controller.CAMERA_PIVOT.THIRD_PERSON)
+
 
 func _physics_process(delta: float) -> void:
 
@@ -56,6 +66,9 @@ func _physics_process(delta: float) -> void:
 
 	# _move_direction is applied to velocity. Check to see if we're in climbing mode.
 	falling(delta)
+
+	if _climbing_mode:
+		update_climbing_anim()
 
 	move_and_check_stuck()
 
@@ -81,7 +94,11 @@ func climbing_raycast() -> void:
 func climbing_shapecast() -> void:
 	var space_state = get_world_3d().direct_space_state
 	var facing := _rotation_root.transform.basis * -Vector3.FORWARD
-	var q = PhysicsRayQueryParameters3D.create(global_position+Vector3.UP, global_position+Vector3.UP+facing)
+
+	var q_from := global_position+Vector3.UP
+	var q_to := global_position+Vector3.UP+facing
+
+	var q = PhysicsRayQueryParameters3D.create(q_from, q_to)
 	q.exclude = [self]
 	var r = space_state.intersect_ray(q)
 
@@ -89,11 +106,44 @@ func climbing_shapecast() -> void:
 		if not r.is_empty():
 			DebugDraw.draw_box(r.position, Vector3(0.2, 0.2, 0.2), Color(0,0,1))
 			wall_normal = r.normal
+			draw_wall_angles(r.position, wall_normal)
 		if Input.is_action_just_pressed("use"):
 			_climbing_mode = not _climbing_mode
+			enter_climbing_anim()
 	elif _climbing_mode:
 		_climbing_mode = false
+		exit_climbing_anim()
 
+# wpoint is a point on the wall in front of the player, from a raycast.
+# wnorm is the normal of the surface of the wall where wpoint lands.
+func draw_wall_angles(wpoint: Vector3, wnorm: Vector3) -> void:
+	# blue line is just wnorm, the normal of the wall
+	DebugDraw.draw_line(wpoint, wpoint+wnorm, Color(0,0,1))
+
+	# red line is the cross-product of wnorm and universal y axis, aka "up." produces a vector
+	#   directly LEFT of the player.
+	# could be changed to opposite of gravity direction for space gameplay, ie mario galaxy or outer wilds? 
+	DebugDraw.draw_line(wpoint, wpoint+(wnorm.cross(Vector3.UP)), Color(1,0,0))
+
+	# green line is what I want, the angle that the player's body should be oriented on the wall.
+	# it is the cross product of the red and blue lines.
+	DebugDraw.draw_line(wpoint, wpoint+(wnorm.cross(Vector3.UP).cross(wnorm)), Color(0,1,0))
+
+# func get_wall_relative_up(normal: Vector3) -> Vector3:
+	# return normal.cross(Vector3.UP).cross(normal)
+
+# This overload uses the global wall_normal, probably bad. sigh
+func get_wall_relative_up() -> Vector3:
+	return wall_normal.cross(Vector3.UP).cross(wall_normal)
+
+func enter_climbing_anim() -> void:
+	pass
+
+func update_climbing_anim() -> void:
+	pass
+
+func exit_climbing_anim() -> void:
+	pass
 
 # Checks if _ground_shapecast is hitting anything. If it is, set _ground_height to the heighest contact point. 
 func calculate_ground_height() -> void:
@@ -185,7 +235,8 @@ func _get_camera_oriented_input() -> Vector3:
 # Rotates _rotation_root to face Vector3 direction.
 func _orient_character_to_direction(direction: Vector3, delta: float) -> void:
 	var left_axis := Vector3.UP.cross(direction)
-	var rotation_basis := Basis(left_axis, Vector3.UP, direction).get_rotation_quaternion()
+	var up_axis := get_wall_relative_up() if _climbing_mode else Vector3.UP
+	var rotation_basis := Basis(left_axis, up_axis, direction).get_rotation_quaternion()
 	var model_scale := _rotation_root.transform.basis.get_scale()
 	_rotation_root.transform.basis = Basis(_rotation_root.transform.basis.get_rotation_quaternion().slerp(rotation_basis, delta * rotation_speed)).scaled(
 		model_scale
